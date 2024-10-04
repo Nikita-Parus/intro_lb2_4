b83cab717c2f462cd4e2c6bf10709d258a5eb59d
Nikita-Parus <cheems.nz@gmail.com>
2024-10-04 19:30:19 +0300

    Добавьте файлы проекта.

---
+++ intro_lb2_4/intro_lb2_4.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int is_prime(int a) {
+    int c = 0;
+    for (int i = 1; i < a; i++) {
+        if(a % i == 0) c++;
+    }
+    return c > 1 ? 0 : 1;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    for (int i = 1; i <= n; i++) {
+        if (is_prime(i)) cout << i << endl;
+    }
+}
